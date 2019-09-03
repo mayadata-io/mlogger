538521f718e6b3db8c33dfbf0c0871a23d9b2fe0
moteesh <moteesh@gmail.com>
2019-09-03 14:23:27 +0530

    reset

--- main.go
+++ main.go
@@ -1,24 +1,21 @@
 package main
-
 import (
-  "go.uber.org/zap"
-  "github.com/Sirupsen/logrus"
-  "github.com/golang/glog"
-  "github.com/go-kit/kit/log/level"
-  "time"
+  "github.com/mayadata-io/mlogger/glog"
 )
 
 func main() {
-  logger, _ := zap.NewProduction()
-  defer logger.Sync() // flushes buffer, if any
-  sugar := logger.Sugar()
-  sugar.Infow("failed to fetch URL",
-    // Structured context as loosely typed key-value pairs.
-    "url", url,
-    "attempt", 3,
-    "backoff", time.Second,
-  )
-  sugar.Infof("Failed to fetch URL: %s", url)
+  //logger, _ := zap.NewProduction()
+  //defer logger.Sync() // flushes buffer, if any
+  //sugar := logger.Sugar()
+  //sugar.Infow("failed to fetch URL",
+  //  // Structured context as loosely typed key-value pairs.
+  //  "url", url,
+  //  "attempt", 3,
+  //  "backoff", time.Second,
+  //)
+  //sugar.Infof("Failed to fetch URL: %s", url)
+  //glog.init()
+  glog.Info("hi")
 }
 
 
