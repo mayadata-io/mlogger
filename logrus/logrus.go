e1fdc12b56565747cc281fe3f2a76daa0de8ef0f
moteesh <moteesh@gmail.com>
2019-09-13 13:54:29 +0530

    fix logrus

--- logrus/logrus.go
+++ logrus/logrus.go
@@ -198,20 +198,20 @@ func Fatalln(args ...interface{}) {
 const (
 	// PanicLevel level, highest level of severity. Logs and then calls panic with the
 	// message passed to Debug, Info, ...
-	PanicLevel = lrs.PanicLevel
+	PanicLevel Level = Level(lrs.PanicLevel)
 	// FatalLevel level. Logs and then calls `logger.Exit(1)`. It will exit even if the
 	// logging level is set to Panic.
-	FatalLevel = lrs.FatalLevel
+	FatalLevel = Level(lrs.FatalLevel)
 	// ErrorLevel level. Logs. Used for errors that should definitely be noted.
 	// Commonly used for hooks to send errors to an error tracking service.
-	ErrorLevel = lrs.ErrorLevel
+	ErrorLevel = Level(lrs.ErrorLevel)
 	// WarnLevel level. Non-critical entries that deserve eyes.
-	WarnLevel = lrs.WarnLevel
+	WarnLevel = Level(lrs.WarnLevel)
 	// InfoLevel level. General operational entries about what's going on inside the
 	// application.
-	InfoLevel = lrs.InfoLevel
+	InfoLevel = Level(lrs.InfoLevel)
 	// DebugLevel level. Usually only enabled when debugging. Very verbose logging.
-	DebugLevel = lrs.DebugLevel
+	DebugLevel = Level(lrs.DebugLevel)
 	// TraceLevel level. Designates finer-grained informational events than the Debug.
 	//TraceLevel = lrs.TraceLevel
 )
